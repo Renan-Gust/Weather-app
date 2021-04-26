72473cbb0845862c432ee75d7940292ece5ed61c
Renan-Gust <renandevfullstack@gmail.com>
2021-04-26 18:07:41 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# App de clima
+
+Um projeto que busca o clima da região que o usuário digitou.
+
+### Ideia daqui
+https://github.com/Renan-Gust/app-ideas
+
+# Tecnologias Utlizadas
+#### [CSS](https://developer.mozilla.org/pt-BR/docs/Web/CSS)
+#### [HTML](https://html.com/)
+#### [JavaScript](https://www.javascript.com/)
+
+## 🚀 Let's code! 🚀
---
+++ app.js
@@ -0,0 +1,47 @@
+const searchBar = document.querySelector(".search-bar")
+
+const api = {
+    key: "3c23c145c9f54acc631857e3cf7de47e",
+    fetchWeather: function(city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.key)
+            .then((response) => {
+                if(!response.ok) alert("Weather not found")
+
+                return response.json()
+            })
+            .then((data) => this.weatherFunction(data))
+    },
+
+    weatherFunction: function(data) {
+        const wind = data.wind.speed
+        const cityName = data.name
+        const { temp, humidity } = data.main
+        const { icon, description } = data.weather[0]
+
+        const fahrenheitToCelsius = Math.ceil(temp - 273)
+
+        document.querySelector(".city").innerHTML = `Weather in ${cityName}`
+        document.querySelector(".temp").innerHTML = fahrenheitToCelsius + "°C"
+        document.querySelector(".description").innerHTML = description
+        document.querySelector(".humidity").innerHTML = `Humidity: ${humidity}%`
+        document.querySelector(".wind").innerHTML = `Wind speed: ${wind} km/h`
+
+        document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}.png`;
+
+        document.querySelector(".weather").classList.remove("loading")
+    },
+
+    search: function() {
+        this.fetchWeather(searchBar.value)
+    }
+}
+
+document.querySelector("button").addEventListener("click", () => {
+    api.search()
+})
+
+searchBar.addEventListener("keyup", (event) => {
+    if(event.keyCode == 13){
+        api.search()
+    }
+})
